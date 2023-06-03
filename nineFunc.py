4e6a3bd29e67e1aca85978bf6024a7bc21a86192
Xnoppanat <nine.noppanat@gmail.com>
2023-06-03 16:59:53 +0700

    math function

--- nineFunc.py
+++ nineFunc.py
@@ -2,3 +2,19 @@ def nine():
     print("Hello Nine")
     return
 
+def add(a,b):
+    print(a + b)
+    return
+
+def sub(a,b):
+    print(a - b)
+    return
+
+def mul(a,b):
+    print(a * b)
+    return
+
+def div(a,b):
+    print(a / b)
+    return
+
